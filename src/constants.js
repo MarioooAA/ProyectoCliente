6dab01c13452c2e02383cb3b1c879023003a1613
Mario Alvarez <mariooowiii01@gmail.com>
2024-11-11 11:13:05 +0100

    commit

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 const COMPANY='IES PIO BAROJA'
+const MODULE='Programacion en Entorno Cliente'
--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
-import { COMPANY } from "./constants.js";
+import { COMPANY, MODULE } from "./constants.js";
 
 console.log('Hola Mundo '+ COMPANY)
+console.log('Hola Mundo '+ MODULE)
+
+import constants from "./constants.js";
+
+console.log('Compañia '+ constants.COMPANY)
+console.log('Departamentos '+ constants.MODULE)
